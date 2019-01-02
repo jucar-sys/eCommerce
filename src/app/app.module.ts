a5dc4b601a0cb4d35c4bd64a48e95e30b7e273aa
JuanCarlosGarcia <jucar.sys@gmail.com>
2019-01-01 19:17:47 -0600

    Creacion de servicio de conexión con API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './componentes/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// MODULOS AGREGADOS MANUALMENTE PARA ÉSTE PROYECTO
+import { HttpClientModule } from '@angular/common/http';
+
+// SERVICIOS
+import { HttpService } from './http.service';
+
+// COMPONENTES
+import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    // COMPONENTES
     LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // MODULOS AGREGADOS MANUALMENTE PARA ÉSTE PROYECTO
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    // SERVICIOS GLOBALES
+    HttpService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/http.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  baseURL = 'https://ecommercenu.firebaseio.com/';
+
+  // Obtener datos de Firebase
+  getDatos(base) {
+    return this.http.get(`${this.baseURL}${base}.json`);
+  }
+
+  // Enviar datos a Firebase
+  sendDatos(datos: any, base) {
+    const data = JSON.stringify(datos);
+    console.log(data);
+    return this.http.put(`${this.baseURL}${base}.json`, data);
+  }
+}
